9b58c0446cb94b4944ecfe93498cceefe8c141d0
Lenny Louis <lenny.louis35@gmail.com>
2023-05-23 13:12:49 -0400

    chages

--- test.py
+++
@@ -1,6 +0,0 @@
-
-dollar = float(input("How much is the meal? ").lstrip("$"))
-percent = float(input("What percent would you like to tip? ").rstrip("%"))
-tip = dollar * (percent / 100)
-total = dollar + tip
-
